5d3442d44a5af606f46e212863fdae5d769e1ff8
Albert Ramirez <ramirezalbert3@gmail.com>
2016-10-19 03:43:24 +0200

    Stager rearrangement for clarity

--- stager.py
+++ stager.py
@@ -3,18 +3,23 @@ class Stager:
         self.limCycles = limCycles
         self.outputFrequency = outputFrequency
         self.printFrequency = printFrequency
-        self.outputFile = open(outputFileName, 'w')
-        outputString = 'Cycles Movements_cycle Visited_states\n'
-        self.outputFile.write(outputString)
         self.cyclesPassed = 0
         self.movementCount = 0
         self.averageMoves = 0
-
-    def outputToFile(self):
-        outputString = '{} {}\n'.format(self.cyclesPassed, self.averageMoves)
+        self.outputFile = open(outputFileName, 'w')
+        outputString = 'Cycles Movements_cycle Visited_states\n'
         self.outputFile.write(outputString)
 
+    def outputToFile(self):
+        if((self.cyclesPassed % self.outputFrequency) != 0):
+            return
+        outputString = '{} {}\n'.format(self.cyclesPassed, self.averageMoves)
+        self.outputFile.write(outputString)
+        self.averageMoves = 0
+
     def printStatus(self):
+        if((self.cyclesPassed % self.printFrequency) != 0):
+            return
         print 'Cycle:', self.cyclesPassed, '\tAverage Moves:', self.averageMoves
 
     def cycleStats(self):
@@ -30,11 +35,8 @@ class Stager:
     def updateCycle(self):
         self.cyclesPassed += 1
         self.cycleStats()
-        if((self.cyclesPassed % self.printFrequency) == 0):
-            self.outputToFile()
-            if((self.cyclesPassed % self.outputFrequency) == 0):
-                self.printStatus()
-            self.averageMoves = 0
+        self.outputToFile()
+        self.printStatus()
         self.movementCount = 0
 
     def areWeCycling(self):
