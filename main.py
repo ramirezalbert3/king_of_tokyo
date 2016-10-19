5b33250eff6a8769cb2fb8f88e09021e25bf9cd9
Albert Ramirez <ramirezalbert3@gmail.com>
2016-10-19 04:54:55 +0200

    EpochHandler initial commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import Stager
 
 
 student = KingAgent(0.7, 0.1, 0.8)
-stager = Stager(2500, 10, 'Output.txt', 250)
+stager = Stager(5000, 10, 'Output.txt', 200)
 
 
 # Training vars
@@ -19,4 +19,4 @@ while(stager.areWeCycling()):
             if(student.remainingRolls == 0):
                 student.setPlayerTurn()
             playing = not (student.didPlayerWin() or student.didPlayerLose())
-    stager.updateCycle()
+    stager.updateCycle(student)
--- utils.py
+++ utils.py
@@ -1,8 +1,47 @@
 import random
 
 
+class epochHandler:
+    def __init__(self, totalCycles=3000, explorationFraction=0.4, trainingFraction=0.5):
+        assert (explorationFraction + trainingFraction) <= 1
+        self.explorationCycles = totalCycles * explorationFraction
+        self.trainingCycles = totalCycles * (explorationFraction + trainingFraction)
+        self.stage = 0
+
+    def setExploration(self, agent):
+        print 'Starting exploration!'
+        self.stage = 1
+        agent.epsilon = 0.5
+        agent.alpha = 1
+        agent.gamma = 0.8
+
+    def setTraining(self, agent):
+        print 'Starting training!'
+        self.stage = 2
+        agent.epsilon = 0.1
+        agent.alpha = 0.7
+        agent.gamma = 0.8
+
+    def setPlay(self, agent):
+        print 'Starting play phase!'
+        self.stage = 3
+        agent.epsilon = 0
+        agent.alpha = 0.1
+        agent.gamma = 0.8
+
+    def stageMonitoring(self, cyclesPassed, agent):
+        if(cyclesPassed < self.explorationCycles):
+            if(self.stage == 0):
+                self.setExploration(agent)
+        elif(cyclesPassed < self.trainingCycles):
+            if(self.stage == 1):
+                self.setTraining(agent)
+        elif(self.stage == 2):
+            self.setPlay(agent)
+
+
 class Stager:
-    def __init__(self, limCycles=2500, outputFrequency=25, outputFileName='Output.txt', printFrequency=250):
+    def __init__(self, limCycles=3000, outputFrequency=10, outputFileName='Output.txt', printFrequency=200):
         self.limCycles = limCycles
         self.outputFrequency = outputFrequency
         self.printFrequency = printFrequency
@@ -10,7 +49,12 @@ class Stager:
         self.movementCount = 0
         self.averageMoves = 0
         self.outputFile = open(outputFileName, 'w')
-        outputString = 'Cycles Movements_cycle Visited_states\n'
+        outputString = 'Cycles Movements_cycle '
+        self.outputFile.write(outputString)
+        self.epochHandler = epochHandler(limCycles, 0.4, 0.5)
+        explCycles = int(self.epochHandler.explorationCycles)
+        trainCycles = int(self.epochHandler.trainingCycles)
+        outputString = '{} {}\n'.format(explCycles, trainCycles)
         self.outputFile.write(outputString)
 
     def outputToFile(self):
@@ -35,11 +79,12 @@ class Stager:
     def updateCount(self):
         self.movementCount += 1
 
-    def updateCycle(self):
+    def updateCycle(self, agent):
         self.cyclesPassed += 1
         self.cycleStats()
         self.outputToFile()
         self.printStatus()
+        self.epochHandler.stageMonitoring(self.cyclesPassed, agent)
         self.movementCount = 0
 
     def areWeCycling(self):
