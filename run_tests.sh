13a32da27515f57a0b46189d898581ea0303ae44
Albert Ramirez <ramirezalbert3@gmail.com>
2016-10-26 23:48:03 +0200

    Adding utils to coverage report, need to design tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@ coverage erase
 echo "Running unit tests"
 coverage run -m unittest discover
 echo -e "\nCoverage report"
-coverage report -m dice.py player.py agent.py kingAgent.py
+coverage report -m dice.py player.py agent.py kingAgent.py utils.py
