7ed2fdca553be498d17df72a30e72ed65239c465
Albert Ramirez <ramirezalbert3@gmail.com>
2016-10-27 00:37:34 +0200

    Turn Handler test implemented

--- testUtils.py
+++ testUtils.py
@@ -1,8 +1,37 @@
-from stager import Stager
+from utils import Stager
+from utils import Monitor
+from utils import EpochHandler
+from utils import TurnHandler
+from constants import ROLLS_PER_TURN
+from kingAgent import KingAgent
 import unittest
 
 
+class TestTurnHandler(unittest.TestCase):
+    def testTurnHandler(self):
+        expectedPlayers = 3
+        auxPlayer1 = KingAgent()
+        auxPlayer2 = KingAgent()
+        auxPlayer3 = KingAgent()
+        playerList = [auxPlayer1, auxPlayer2, auxPlayer3]
+        playerList[0].setPlayerTurn()
+        testedHandler = TurnHandler()
+        testedHandler.addPlayers(playerList)
+        self.assertEqual(testedHandler.numberOfPlayers, expectedPlayers)
+        for loops in range(50):
+            for player in playerList:
+                    player.resetPlayer()
+            playerList[0].setPlayerTurn()
+            testedHandler.playingID = 0
+            for i in range(loops):
+                for player in playerList:
+                    player.act()
+                testedHandler.setTurn(playerList)
+            playerPlaying = (loops / ROLLS_PER_TURN) % expectedPlayers
+            self.assertEqual(testedHandler.playingID, playerPlaying)
+
+
 class TestStager(unittest.TestCase):
-    def TestStager(self):
+    def testStager(self):
         testedStager = Stager()
         pass
